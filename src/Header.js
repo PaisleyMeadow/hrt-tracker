2686c5e82d7d74fe7b2c7730aad0e36c46b2b9b8
PaisleyMeadow <paisley1000@gmail.com>
2021-01-23 20:02:24 -0700

    there is a react header oh boy (default react code)

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './header.css';
+
+function Header() {
+    return(
+        <div className="Header">
+            <h1 className="app-logo">TransTrack</h1>
+        </div>
+    );
+}
+
+export default Header;
