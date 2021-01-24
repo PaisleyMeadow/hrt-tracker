2125ff6f2ba1d1bac153a3807378a2b642606c35
Anna Rift <ultimateartificer@gmail.com>
2021-01-24 04:10:51 -0700

    Add data value collection for Community data

--- src/Components/Community.jsx
+++ src/Components/Community.jsx
@@ -33,30 +33,70 @@ function Community() {
 class DataInput extends React.Component {
     constructor(props) {
         super(props);
+
+        // input handling inspired by https://dmitripavlutin.com/react-forms-tutorial/
+        // turned out to be an awful way of doing it lmao
+        this.state = {
+            values: {
+                hrttime: "",
+                age: "",
+                form: "",
+                libido: false,
+                acne: false,
+                weightgain: false,
+                weightloss: false,
+                muscle: false,
+                fatredistribution: false,
+                irritability: false,
+                bodyhair: false,
+                facialhair: false,
+                voicechanges: false,
+                bodyodor: false,
+                bottomgrowth: false,
+                othereffects: ""
+            }
+        };
+
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.setData = this.setData.bind(this);
     }
 
     handleSubmit(e) {
         console.log("Submitting information form");
         e.preventDefault();
+        console.log(this.state.values);
+        alert("Information submitted!");
+        window.location.reload();
     }
+
+    setData(name, isCheckbox = false) {
+        return ({ target: { value } }) => {
+            let newValue;
+            if (isCheckbox) {
+                newValue = (value === "on");
+            } else {
+                newValue = value;
+            }
+            this.state.values[name] = newValue;
+        };
+    };
 
     render() {
         return (
-            <div className="dataInput">
+            <div>
                 <h1>Submit information</h1>
                 <form onSubmit={this.handleSubmit}>
                     <label>General info</label>
                     <br/>
                     <label>Time on HRT (months, approx):</label>
-                    <input type="text" name="hrttime" />
+                    <input onChange={this.setData("hrttime")} type="text" name="hrttime" />
                     <br/>
                     <label>Age:</label>
-                    <input type="text" name="age" />
+                    <input onChange={this.setData("age")} type="text" name="age" />
                     <br/>
                     <label>Form:</label>
-                    <select>
-                        <option value={null}>select...</option>
+                    <select onChange={this.setData("form")} name="form">
+                        <option value={null} selected>select...</option>
                         <option value="pills">Pills</option>
                         <option value="gel">Gel/cream</option>
                         <option value="patch">Patch</option>
@@ -66,43 +106,43 @@ class DataInput extends React.Component {
                     <label>Testosterone-specific info</label>
                     <br/>
                     <label>Increased libido:</label>
-                    <input type="checkbox" name="libido"/>
+                    <input onChange={this.setData("libido", true)} type="checkbox" name="libido"/>
                     <br/>
                     <label>Increased acne:</label>
-                    <input type="checkbox" name="acne"/>
+                    <input onChange={this.setData("acne", true)} type="checkbox" name="acne"/>
                     <br/>
                     <label>Weight gain:</label>
-                    <input type="checkbox" name="weightgain"/>
+                    <input onChange={this.setData("weightgain", true)} type="checkbox" name="weightgain"/>
                     <br/>
                     <label>Weight loss:</label>
-                    <input type="checkbox" name="weightloss"/>
+                    <input onChange={this.setData("weightloss", true)} type="checkbox" name="weightloss"/>
                     <br/>
                     <label>Muscle gain:</label>
-                    <input type="checkbox" name="muscle"/>
+                    <input onChange={this.setData("muscle", true)} type="checkbox" name="muscle"/>
                     <br/>
                     <label>Fat redistribution:</label>
-                    <input type="checkbox" name="fatredistribution"/>
+                    <input onChange={this.setData("fatredistribution", true)} type="checkbox" name="fatredistribution"/>
                     <br/>
                     <label>Increased irritability:</label>
-                    <input type="checkbox" name="irritability"/>
+                    <input onChange={this.setData("irritability", true)} type="checkbox" name="irritability"/>
                     <br/>
                     <label>Increased body hair:</label>
-                    <input type="checkbox" name="bodyhair"/>
+                    <input onChange={this.setData("bodyhair", true)} type="checkbox" name="bodyhair"/>
                     <br/>
                     <label>Increased facial hair:</label>
-                    <input type="checkbox" name="facialhair"/>
+                    <input onChange={this.setData("facialhair", true)} type="checkbox" name="facialhair"/>
                     <br/>
                     <label>Voice changes:</label>
-                    <input type="checkbox" name="voicechanges"/>
+                    <input onChange={this.setData("voicechanges", true)} type="checkbox" name="voicechanges"/>
                     <br/>
                     <label>Body odor changes:</label>
-                    <input type="checkbox" name="bodyodor"/>
+                    <input onChange={this.setData("bodyodor", true)} type="checkbox" name="bodyodor"/>
                     <br/>
                     <label>Bottom growth:</label>
-                    <input type="checkbox" name="bottomgrowth"/>
+                    <input onChange={this.setData("bottomgrowth", true)} type="checkbox" name="bottomgrowth"/>
                     <br/>
                     <label>Other effects:</label>
-                    <input type="text" name="othereffects"/>
+                    <input onChange={this.setData("othereffects")} type="text" name="othereffects"/>
                     <br/>
                     <input type="submit" value="submit" />
                 </form>
