73c75a2d442b9797b0e8dbc0527b03c1b2b665dd
Anna Rift <ultimateartificer@gmail.com>
2021-01-23 22:58:11 -0700

    Remove default React stuff

--- src/App.js
+++ src/App.js
@@ -6,20 +6,6 @@ import { Navigation, Home, Signup, Profile, Community } from "./Components";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Router>
         <Navigation />
         <Switch>
